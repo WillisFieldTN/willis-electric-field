c6c4b6e80e2f48e874ad57773766117396a7697c
WillisFieldTN <abaker240@gmail.com>
2026-02-10 17:23:17 -0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+document.getElementById("dispatchBtn").addEventListener("click", () => {
+  alert("Dispatch screen coming soon!");
+});
+
+document.getElementById("customersBtn").addEventListener("click", () => {
+  alert("Customers screen coming soon!");
+});
+
+document.getElementById("inventoryBtn").addEventListener("click", () => {
+  alert("Inventory screen coming soon!");
+});
+
+document.getElementById("searchBtn").addEventListener("click", () => {
+  alert("Equipment search coming soon!");
+});
